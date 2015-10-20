249c07e1ab63054039ea220df98723e1c9875701
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2015-10-20 10:20:41 -0500

    First Commit

---
+++ Stack_Example_10202015/Program.cs
@@ -0,0 +1,19 @@
+﻿//Joshua Pickenpaugh, IN2017
+//October 20th, 2015
+//Stack Example
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Example_10202015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
