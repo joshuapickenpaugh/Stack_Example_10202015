7f8ab2459139c1366f05a116b2e249df2c7964e1
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2015-10-20 11:10:27 -0500

    Final Commit

--- Stack_Example_10202015/Program.cs
+++ Stack_Example_10202015/Program.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,32 @@ namespace Stack_Example_10202015
     {
         static void Main(string[] args)
         {
+            Stack stack1 = new Stack();
+            stack1.Push(2);
+            stack1.Push(4);
+            stack1.Push(6);
+            stack1.Push(8);
+            stack1.Push(10);
+
+            MethodStack(stack1);
+
+            Console.ReadKey();
         }
-    }
+
+        static void MethodStack(Stack methStack)
+        {
+            if (methStack.Contains(6))
+            {
+                Console.WriteLine("Here is the Stack:");
+            }
+            
+            while (0 < methStack.Count)
+            {
+                Console.WriteLine(methStack.Pop());
+            }
+
+            Console.WriteLine("Press a key to exit program.");
+        }
+    } 
 }
+
